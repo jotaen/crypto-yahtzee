afb2fcaeb85e748cc8f47eab25a8b14b1d58d432
Jan Heuermann <jan@jotaen.net>
2020-04-10 21:38:54 +0200

    Implement randomiser module

---
+++ src/crypto/randomiser.js
@@ -0,0 +1,83 @@
+const { hash } = require("./hash")
+
+const isHexString = length =>
+	x => typeof x === "string" && x.length === length && /^[a-f0-9]*$/.test(x)
+
+const isOkay = entry => {
+	if (!entry.hashes || !entry.values) {
+		return true
+	}
+	if (entry.values.every((v, i) => entry.hashes[i] === hash(v))) {
+		return true
+	}
+	return false
+}
+
+const VALUE_STRING_LENGTH = 8 // 32 bit hex value
+
+class ConcertedRandomiser {
+	constructor(arity, participants) {
+		this._arity = arity
+		this._entries = participants.reduce((a, c) => {
+			a[c] = { hashes: null, values: null }
+			return a
+		}, {})
+	}
+
+	_submit(kind, validate, participant, ds) {
+		if (!Array.isArray(ds) || ds.length !== this._arity) {
+			throw "WRONG_ARITY"
+		}
+		if (!ds.every(validate)) {
+			throw "MALFORMED_VALUE"
+		}
+		if (! (participant in this._entries)) {
+			throw "NOT_PARTICIPANT"
+		}
+		if (this._entries[participant][kind] !== null) {
+			throw "ALREADY_SUBMITTED"
+		}
+		const candidate = { ...this._entries[participant], [kind]: ds }
+		if (!isOkay(candidate)) {
+			throw "HASH_VALUE_MISMATCH"
+		}
+		this._entries[participant] = candidate
+	}
+
+	submitHashes(participant, hashes) {
+		this._submit("hashes", isHexString(64), participant, hashes)
+	}
+
+	submitValues(participant, values) {
+		this._submit("values", isHexString(VALUE_STRING_LENGTH), participant, values)
+	}
+
+	retrieveNumber() {
+		const allValues = Object.values(this._entries)
+			.map(e => e.values)
+
+		return Array.from(Array(this._arity))
+			.map((_, i) => allValues
+					.map(vs => vs[i])
+					.map(v => parseInt(v, 16))
+					.reduce((a, c) => a ^ c, 0)
+			)
+	}
+}
+
+// Generates 32-bit random numbers with corresponding hash
+const random = () => {
+	const value = Math.random()
+		.toString(16)
+		.substr("0.".length) // float prefix
+		.padEnd(VALUE_STRING_LENGTH, "0")
+		.substr(0, VALUE_STRING_LENGTH)
+	return {
+		hash: hash(value),
+		value: value,
+	}
+}
+
+module.exports = {
+	ConcertedRandomiser, random, VALUE_STRING_LENGTH
+}
---
+++ src/crypto/randomiser.spec.js
@@ -0,0 +1,141 @@
+const assert = require("assert")
+const { ConcertedRandomiser, random, VALUE_STRING_LENGTH } = require("./randomiser")
+
+describe("[Randomiser] Generation", () => {
+	it("takes data for every player", () => {
+		const s = new ConcertedRandomiser(2, ["ALICE", "BOB", "CHRIS"])
+		s.submitHashes("ALICE", [random().hash, random().hash])
+		s.submitHashes("BOB", [random().hash, random().hash])
+		s.submitHashes("CHRIS", [random().hash, random().hash])
+	})
+
+	it("cannot overwrite values", () => {
+		const s = new ConcertedRandomiser(1, ["ALICE", "BOB", "CHRIS"])
+		const alice = random()
+		s.submitHashes("ALICE", [alice.hash])
+		s.submitValues("ALICE", [alice.value])
+		assert.throws(
+			() => s.submitHashes("ALICE", [random().hash]),
+			e => e === "ALREADY_SUBMITTED"
+		)
+		assert.throws(
+			() => s.submitValues("ALICE", [random().value]),
+			e => e === "ALREADY_SUBMITTED"
+		)
+	})
+
+	it("rejects formally incorrect values/types", () => {
+		const s = new ConcertedRandomiser(3, ["ALICE", "BOB", "CHRIS"])
+		assert.throws(
+			() => s.submitHashes("ALICE", ["", "!*&^TGAIS*F&", 2127]),
+			e => e === "MALFORMED_VALUE"
+		)
+		assert.throws(
+			() => s.submitValues("ALICE", ["", "*&T@*&UR!SFS", 5123]),
+			e => e === "MALFORMED_VALUE"
+		)
+	})
+
+	it("cannot submit data for non-participant", () => {
+		const s = new ConcertedRandomiser(2, ["ALICE", "BOB", "CHRIS"])
+		assert.throws(
+			() => s.submitHashes("MALICIOUS", [random().hash, random().hash]),
+			e => e === "NOT_PARTICIPANT"
+		)
+		assert.throws(
+			() => s.submitValues("MALICIOUS", [random().value, random().value]),
+			e => e === "NOT_PARTICIPANT"
+		)
+	})
+
+	it("rejects adding hashes with wrong arity", () => {
+		const s = new ConcertedRandomiser(3, ["ALICE"])
+		assert.throws(
+			() => s.submitHashes("ALICE", null),
+			e => e === "WRONG_ARITY"
+		)
+		assert.throws(
+			() => s.submitHashes("ALICE", "aced120"),
+			e => e === "WRONG_ARITY"
+		)
+		assert.throws(
+			() => s.submitHashes("ALICE", []),
+			e => e === "WRONG_ARITY"
+		)
+		assert.throws(
+			() => s.submitHashes("ALICE", ["aced120", "aced120"]),
+			e => e === "WRONG_ARITY"
+		)
+		assert.throws(
+			() => s.submitHashes("ALICE", ["aced120", "aced120", "aced120", "aced120"]),
+			e => e === "WRONG_ARITY"
+		)
+		assert.throws(
+			() => s.submitValues("ALICE", null),
+			e => e === "WRONG_ARITY"
+		)
+		assert.throws(
+			() => s.submitValues("ALICE", "101"),
+			e => e === "WRONG_ARITY"
+		)
+		assert.throws(
+			() => s.submitValues("ALICE", []),
+			e => e === "WRONG_ARITY"
+		)
+		assert.throws(
+			() => s.submitValues("ALICE", ["001", "111"]),
+			e => e === "WRONG_ARITY"
+		)
+		assert.throws(
+			() => s.submitValues("ALICE", ["000", "101", "110", "111"]),
+			e => e === "WRONG_ARITY"
+		)
+	})
+
+	it("checks for value integrity", () => {
+		const s = new ConcertedRandomiser(1, ["ALICE", "BOB"])
+		const alice = random()
+		const evil = random()
+		s.submitHashes("ALICE", [alice.hash])
+		assert.throws(
+			() => s.submitValues("ALICE", [evil.value]),
+			e => e === "HASH_VALUE_MISMATCH"
+		)
+
+		const bob = random()
+		s.submitValues("BOB", [bob.value])
+		assert.throws(
+			() => s.submitHashes("BOB", [evil.hash]),
+			e => e === "HASH_VALUE_MISMATCH"
+		)
+	})
+
+	it("returns result when data is complete", () => {
+		const s = new ConcertedRandomiser(2, ["ALICE", "BOB"])
+		s.submitHashes("ALICE", [
+			"fbd0ed2087be3dafb52fd873c8a30a83cdd347c173c752b9fb31897f07ebc76d",
+			"deab94ad3fef33bfac696c5fa5a507044d8ae4138d07a96618caf990f5fba66c"
+		])
+		s.submitValues("ALICE", ["6b2df804", "02713fef"])
+		s.submitHashes("BOB", [
+			"5f53131e72425adfb6a007f61309deeb11ccfa06f0156703079c855e995bd71b",
+			"b736994617eb6d9a573342120f383686447dad1688fb25838dc674ca29b83687"
+		])
+		s.submitValues("BOB", ["7d6d2b55", "bc0150fb"])
+		assert.deepStrictEqual(s.retrieveNumber(), [373347153, -1099927788])
+	})
+})
+
+describe("[Randomiser] random()", () => {
+	it("generates bit sequences as string of specified length", () => {
+		const r = random()
+		assert.strictEqual(r.hash.length, 64)
+		assert.strictEqual(/^[a-f0-9]*$/.test(r.hash), true)
+		assert.strictEqual(r.value.length, VALUE_STRING_LENGTH)
+		assert.strictEqual(/^[a-f0-9]*$/.test(r.value), true)
+	})
+
+	it("yields different outputs every time", () => {
+		assert.notDeepStrictEqual(random(), random())
+	})
+})
