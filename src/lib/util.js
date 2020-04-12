226338acab2e356207c594877fa1b2e9dbfa8bc8
Jan Heuermann <jan@jotaen.net>
2020-04-12 17:25:08 +0200

    Conflate assertions

--- src/lib/util.js
+++ src/lib/util.js
@@ -2,6 +2,18 @@ const deepClone = obj => JSON.parse(JSON.stringify(obj))
 
 const isString = x => typeof x === "string"
 
+const isSubset = (ds, ss) => {
+	const xs = ds.slice()
+	return ss.reduce((a, c) => {
+		const i = xs.indexOf(c)
+		if (i === -1) { a.push(c) }
+		else { xs[i] = undefined }
+		return a
+	}, []).length === 0
+}
+
+const assert = ([msg, assertation]) => { if (assertation()) throw msg; }
+
 module.exports = {
-	isString, deepClone
+	isString, deepClone, isSubset, assert
 }
--- src/yahtzee/game.js
+++ src/yahtzee/game.js
@@ -1,20 +1,6 @@
 const { createScorecard, count, isFilledUp } = require("./scorecard")
 const { isOfShape } = require("../lib/redux")
-const { isString, deepClone } = require("../lib/util")
-
-const isValidDice = d => [1, 2, 3, 4, 5, 6].includes(d)
-
-const isValidSelection = s => [1, 2, 3, 4, 5, 6, null].includes(s)
-
-const isSelectionCompatible = (ds, ss) => {
-	const xs = [null, null, null, null, null].concat(ds)
-	return ss.reduce((a, c) => {
-		const i = xs.indexOf(c)
-		if (i === -1) { a.push(c) }
-		else { xs[i] = undefined }
-		return a
-	}, []).length === 0
-}
+const { isString, deepClone, isSubset, assert } = require("../lib/util")
 
 const init = (players) => ({
 	players: players,
@@ -25,48 +11,35 @@ const init = (players) => ({
 })
 
 const roll = (state, { player, dices }) => {
-	if (state.players[state.onTurn] !== player) {
-		throw "NOT_ON_TURN"
-	}
 	const rollers = state.dices.filter(d => d === null)
 	const remainers = state.dices.filter(d => d !== null)
-	if (rollers.length === 0) {
-		throw "NO_DICES_SELECTED"
-	}
-	if (dices.length !== rollers.length) {
-		throw "INVALID_ROLL"
-	}
+	;[
+		["NOT_ON_TURN", () => state.players[state.onTurn] !== player],
+		["NO_DICES_SELECTED", () => rollers.length === 0],
+		["INVALID_ROLL", () => dices.length !== rollers.length],
+	].forEach(assert)
 	const newDices = remainers.concat(dices).sort((a,b) => a-b)
 	return { ...state, attempt: state.attempt+1, dices: newDices }
 }
 
 const select = (state, { player, dices }) => {
-	if (state.players[state.onTurn] !== player) {
-		throw "NOT_ON_TURN"
-	}
-	if (state.attempt >= 3) {
-		throw "ROLLS_EXCEEDED"
-	}
-	if (state.dices.some(d => d === null)) {
-		throw "ALREADY_SELECTED"
-	}
-	if (!isSelectionCompatible(state.dices, dices)) {
-		throw "INCOMPATIBLE_SELECTION"
-	}
+	const remainers = dices.filter(d => d !== null)
+	;[
+		["NOT_ON_TURN", () => state.players[state.onTurn] !== player],
+		["ROLLS_EXCEEDED", () => state.attempt >= 3],
+		["ALREADY_SELECTED", () => state.dices.some(d => d === null)],
+		["INCOMPATIBLE_SELECTION", () => !isSubset(state.dices, remainers)],
+	].forEach(assert)
 	return { ...state, dices: dices.slice() }
 }
 
 const record = (state, { player, category }) => {
-	if (state.players[state.onTurn] !== player) {
-		throw "NOT_ON_TURN"
-	}
 	const scorecard = state.scorecards[state.onTurn]
-	if (state.dices.some(d => d === null)) {
-		throw "NO_DICES_ROLLED"
-	}
-	if (scorecard[category] !== null) {
-		throw "CATEGORY_ALREADY_RECORDED"
-	}
+	;[
+		["NOT_ON_TURN", () => state.players[state.onTurn] !== player],
+		["NO_DICES_ROLLED", () => state.dices.some(d => d === null)],
+		["CATEGORY_ALREADY_RECORDED", () => scorecard[category] !== null],
+	].forEach(assert)
 	const newState = deepClone(state)
 	const nextOnTurn = state.players.length-1 === state.onTurn ? 0 : state.onTurn + 1
 	newState.scorecards[newState.onTurn][category] = count(state.dices)[category]
@@ -75,6 +48,10 @@ const record = (state, { player, category }) => {
 	newState.dices = [null, null, null, null, null]
 	return newState
 }
+
+const isValidDice = d => [1, 2, 3, 4, 5, 6].includes(d)
+
+const isValidSelection = s => [1, 2, 3, 4, 5, 6, null].includes(s)
 
 const registry = {
 	"ROLL": {
