a19164131d5653932203d27cbf31f8c4b50c8756
Jan Heuermann <jan@jotaen.net>
2020-04-13 16:07:38 +0200

    Sketch out game implementation

--- src/game.js
+++ src/game.js
@@ -1,17 +1,64 @@
 const { DiceCup, toDices } = require("./crypto/dicecup")
+const { Blockchain } = require("./crypto/blockchain")
+const { hash } = require("./crypto/hash")
 const { Yahtzee } = require("./yahtzee/yahtzee")
-const { sortBy } = require("./lib/util")
+const { sortBy, noop } = require("./lib/util")
 
 class Game {
-	constructor(players) {
-		this._storeMachine = StoreMachine(players)
+	constructor(privateKey, otherPlayersPublicKeys, callbacks) {
+		this._blockchain = new Blockchain(privateKey, otherPlayersPublicKeys)
+		this._owner = this._blockchain.owner()
+		this._storePointer = StoreMachine(this._blockchain.participants())
+		this._yahtzee = null
+		this._dices = null
+		this._callbacks = {
+			onRoll: noop,
+			onSelect: noop,
+			onRecord: noop,
+			onPopulateBlock: noop,
+			...callbacks,
+		}
+		this._update()
 	}
 
-	dispatch(action) {
-		this._storeMachine.next().value.dispatch(action)
+	receiveBlock(block) {
+		if (hash(block.payload.player) !== block.author) {
+			return
+		}
+		this._blockchain.stageForeignBlock(this._yahtzee, block)
+		this._storePointer.next().value.dispatch(block.payload)
+		this._blockchain.commit()
+	}
 
-		// invoke generator again to advance/flush internal state
-		return this._storeMachine.next().value
+	select(dices) {
+		this._dispatchOwnAction({
+			type: "SELECT",
+			player: this._owner,
+			dices: dices,
+		})
+	}
+
+	_update() {
+		const currentStore = this._storePointer.next().value
+		if (currentStore instanceof Yahtzee) {
+			this._yahtzee == s
+		}
+		if (currentStore instanceof DiceCup) {
+			this._dices = this._callbacks.onRoll(currentStore.getState().arity)
+			this._dispatchOwnAction({
+				type: "DICECUP_HASHES",
+				player: this._owner,
+				seeds: this._dices.map(d => d.seed),
+				hashes: this._dices.map(d => d.hash),
+			})
+		}
+	}
+
+	_dispatchOwnAction(action) {
+		this._storePointer.next().value.dispatch(action)
+		this._blockchain.stageOwnBlock(this._yahtzee, action)
+		this._blockchain.commit()
+		this._callbacks.onPopulateBlock(this._blockchain.latestBlock())
 	}
 }
 
--- src/game.spec.js
+++ src/game.spec.js
@@ -1,60 +1,25 @@
 const { Game } = require("./game")
+const { random } = require("./crypto/dicecup")
 
-const randomTestData = require("./crypto/random-testdata.json")
-function* Random() {
-	let i = -1
-	while(true) {
-		i++
-		yield randomTestData[i]
-	}
-}
+const { ALICE, BOB, CHRIS } = require("./crypto/rsa-testdata.json")
 
 const hashes = rs => rs.map(r => r.value.hash)
 const values = rs => rs.map(r => r.value.value)
 
+const defaultCallbacks = buffer => ({
+	onRoll: arity => Array.from(Array(arity)).map(() => random()),
+	onPopulateBlock: block => buffer.push(block),
+})
+
 describe("[Game] Entire game flow", () => {
-	const r = Random()
-	const g = new Game(["ALICE", "BOB", "CHRIS"])
+	it("1. Initialisation (determine turn order through rolling)", () => {
+		const buffer = []
+		const game = {
+			alice: new Game(ALICE, [BOB.public], defaultCallbacks(buffer)),
+			bob: new Game(BOB, [ALICE.public], defaultCallbacks(buffer)),
+		}
 
-	const roll = (numberOfDices) => {
-		const alice = Array.from(Array(numberOfDices)).map(() => r.next())
-		const bob = Array.from(Array(numberOfDices)).map(() => r.next())
-		const chris = Array.from(Array(numberOfDices)).map(() => r.next())
-		g.dispatch({ type: "SEED_HASHES", player: "ALICE", hashes: hashes(alice) })
-		g.dispatch({ type: "SEED_HASHES", player: "BOB", hashes: hashes(bob) })
-		g.dispatch({ type: "SEED_HASHES", player: "CHRIS", hashes: hashes(chris) })
-		g.dispatch({ type: "SEED_VALUES", player: "ALICE", values: values(alice) })
-		g.dispatch({ type: "SEED_VALUES", player: "BOB", values: values(bob) })
-		g.dispatch({ type: "SEED_VALUES", player: "CHRIS", values: values(chris) })
-	}
-
-	it("initialises the game to determine the turn order", () => {
-		roll(3)
-	})
-
-	it("takes turns one after the other", () => {
-		roll(5)
-		g.dispatch({ type: "RECORD", player: "BOB", category: "largeStraight" })
-
-		roll(5)
-		g.dispatch({ type: "SELECT", player: "CHRIS", dices: [1, 1, 1, null, null]})
-		roll(2)
-		g.dispatch({ type: "SELECT", player: "CHRIS", dices: [1, 1, 1, null, null]})
-		roll(2)
-		g.dispatch({ type: "RECORD", player: "CHRIS", category: "aces" })
-
-		roll(5)
-		g.dispatch({ type: "SELECT", player: "ALICE", dices: [5, 5, null, null, null]})
-		roll(3)
-		g.dispatch({ type: "SELECT", player: "ALICE", dices: [5, 5, null, null, null]})
-		roll(3)
-		g.dispatch({ type: "RECORD", player: "ALICE", category: "fives" })
-
-		roll(5)
-		g.dispatch({ type: "SELECT", player: "BOB", dices: [3, 3, null, null, null]})
-		roll(3)
-		g.dispatch({ type: "SELECT", player: "BOB", dices: [3, 3, null, null, null]})
-		roll(3)
-		g.dispatch({ type: "RECORD", player: "BOB", category: "smallStraight" })
+		// game.alice.receiveBlock(buffer.pop())
+		// game.bob.receiveBlock(buffer.pop())
 	})
 })
