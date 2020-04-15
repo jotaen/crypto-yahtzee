81465e3708978bc90ea0efd31a5e0c93547d674a
Jan Heuermann <jan@jotaen.net>
2020-04-15 15:48:33 +0200

    Sketch simple server (p2p-broker)

---
+++ src/runtime/broker/index.js
@@ -0,0 +1,23 @@
+const WebSocket = require("ws")
+
+const server = new WebSocket.Server({ port: 8080 })
+
+const clients = new Map()
+
+server.on("connection", (client, request) => {
+  const publicKey = request.searchParams.get("publicKey")
+  clients.set(publicKey, client)
+
+  client.on("message", data => {
+    const message = JSON.parse(data)
+    const recipient = clients.get(message.recipient)
+    if (!recipient || !recipient.readyState === WebSocket.OPEN) {
+      return
+    }
+    recipient.send(JSON.stringify(message.block))
+  })
+
+  client.on("close", () => {
+    clients.delete(publicKey)
+  })
+})
