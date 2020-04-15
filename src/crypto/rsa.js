cbe74654342996f4031178de49bd6c177a3c4594
Jan Heuermann <jan@jotaen.net>
2020-04-15 11:44:52 +0200

    Add fn to generate key pairs

--- src/crypto/rsa.js
+++ src/crypto/rsa.js
@@ -1,5 +1,6 @@
 const crypto = require("crypto")
 const safeStringify = require("json-stable-stringify")
+const util = require("util")
 
 const ALGORITHM = "sha256"
 const SERIALISATION_BASE = "hex"
@@ -13,4 +14,12 @@ module.exports.verify = (block, publicKey) => {
 	const signature = block.signature
 	const json = safeStringify({ ...block, signature: null })
 	return crypto.verify(ALGORITHM, Buffer.from(json), publicKey, Buffer.from(signature, SERIALISATION_BASE))
+}
+
+module.exports.generateKeyPair = () => {
+	return util.promisify(crypto.generateKeyPair)('rsa', {
+		modulusLength: 4096,
+		publicKeyEncoding: { type: "pkcs1", format: "pem" },
+		privateKeyEncoding: { type: "pkcs1", format: "pem" },
+	})
 }
