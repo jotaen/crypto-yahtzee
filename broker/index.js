f08cf50f39bcd302947f8df4d4a1c2e7842060b4
Jan Heuermann <jan@jotaen.net>
2020-04-20 11:50:03 +0200

    Reduce log volume

--- broker/index.js
+++ broker/index.js
@@ -11,7 +11,6 @@ console.log(`Broker listening on ${PORT}`)
 server.on("connection", (client, request) => {
   const clientId = querystring.parse(url.parse(request.url).query).id
   if (!clientId || String(clientId).length < 128 || clients.has(clientId)) {
-    console.log(`Connection attempt failed from '${clientId}'`)
     client.close()
     return
   }
@@ -23,14 +22,10 @@ server.on("connection", (client, request) => {
       const recipientId = JSON.parse(messageTxt).recipient
       const recipient = clients.get(recipientId)
       if (!recipient || !recipient.readyState === WebSocket.OPEN) {
-        console.log(`Cannot dispatch to '${recipientId}'`)
         return
       }
-      console.log(`Dispatch message '${clientId}' > '${recipientId}'`)
       recipient.send(messageTxt)
-    } catch(e) {
-      console.log(`Error dispatching message from '${clientId}'`, e.message)
-    }
+    } catch(e) {}
   })
 
   client.on("close", () => {
